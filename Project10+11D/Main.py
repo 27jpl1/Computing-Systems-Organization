c916fcfeab9610f264dab8cffd0450a9cbf3aee5
JP Lyon <japylyon@gmail.com>
2024-04-22 13:17:39 -0500

    Directories Work

--- Project10+11D/Main.py
+++ Project10+11D/Main.py
@@ -1,11 +1,20 @@
 from Compiler import *
 import sys
+import os
+
+def compile_file(file):
+    vm_file_name = file.split('.')[0] + '.vm'
+    compiler = Compiler(file, vm_file_name)
+    compiler.compile_class()
 
 def main():
-    jack_file_name = sys.argv[1]
-    vm_file_name = jack_file_name.split('.')[0] + 'vm'
-    compiler = Compiler(jack_file_name, vm_file_name)
-    compiler.compile_class()
+    input = sys.argv[1]
+    if os.path.isdir(input):
+        for file in os.listdir(input):
+            if ".jack" in file:
+                compile_file(os.path.join(input, file))
+    else:
+        compile_file(input)
 
 
 main()
